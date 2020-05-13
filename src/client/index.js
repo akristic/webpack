0c276cecb1ff03c99f34e626acd94da3a72b541f
Ante Kristic <ante.kristic81@gmail.com>
2020-05-13 10:11:19 +0200

    Fixing Functionality

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,13 @@
 import { handleSubmit } from './js/formHandler'
 import { checkForName } from './js/nameChecker'
+
+import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 import './styles/_footer.scss'
-console.log(checkForName);
 
-alert('It works!')
+export {
+    checkForName,
+    handleSubmit
+}
